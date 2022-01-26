7ba11f9e942653de10179e0687679f738264ce5c
victorponz <victorponz@gmail.com>
2022-01-26 11:36:36 +0100

    Añadir suma, controlado y test unitario

---
+++ src/main/java/com/victor/calculator/Calculator.java
@@ -0,0 +1,9 @@
+package com.victor.calculator;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+     int sum(int a, int b) {
+          return a + b;
+     }
+}
